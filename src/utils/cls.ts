592dfa4075541f22e7d003ccf43f4f081088e34e
junjeeong <wn8624@naver.com>
2025-02-14 14:12:10 +0900

    merge dev

--- src/utils/cls.ts
+++ src/utils/cls.ts
@@ -1,6 +1,4 @@
 // 여러 문자열의 className을 인자로 받아 하나의 className으로 반환해주는 함수
-const cls = (...classses: (string | undefined | false)[]) => {
+export const cls = (...classses: (string | undefined | false)[]) => {
   return classses.filter(Boolean).join(" ");
 };
-
-export default cls;
