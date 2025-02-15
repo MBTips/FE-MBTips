65740c9e9266badbc73069bfd9ff8bc054b4cb86
junjeeong <wn8624@naver.com>
2025-02-15 14:39:08 +0900

    feat: HOME 페이지 컴포넌트 작업중 -> 컴포넌트 배치

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,19 @@
+import Banner from "@/components/Banner";
+import PrimaryButton from "@/components/button/PrimaryButton";
+import NavBar from "@/components/NavBar";
+import StrokeBanner from "@/components/StrokeBanner";
+
 const Home = () => {
-  return ()
-}
+  return (
+    <div>
+      <NavBar />
+      <Banner />
+      <SubTitle mode="빠른대화" />
+      <PrimaryButton size="md">대화 시작하기</PrimaryButton>
+      <SubTitle mode="친구목록" />
+      <StrokeBanner />
+    </div>
+  );
+};
 
 export default Home;
