62155792f68e7d12e85c653901dda2b6706e411e
junjeeong <wn8624@naver.com>
2025-03-18 16:21:39 +0900

    style: tailwind.config.ts 추가 -> index.css에 @theme 값들 옮김

--- src/components/button/ChatStartButton.tsx
+++ src/components/button/ChatStartButton.tsx
@@ -1,11 +1,14 @@
-import PrimaryButton from "@/components/button/PrimaryButton";
+import { MouseEvent } from "react";
 
-const ChatStartButton = () => {
-    return (
-        <PrimaryButton size="md">
-            대화 시작하기
-      </PrimaryButton>
-  )
-}
+const ChatStartButton = ({ onClick }: { onClick: (e: MouseEvent) => void }) => {
+  return (
+    <button
+      className="flex h-[56px] w-[320px] items-center justify-center rounded-lg bg-primary-normal text-white md:w-[335px] lg:w-[460px]"
+      onClick={onClick}
+    >
+      대화 시작하기
+    </button>
+  );
+};
 
 export default ChatStartButton;
