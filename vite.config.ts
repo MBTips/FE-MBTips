f58abf2695724391b6309074cc253bfee02deac6
yuhojae <ho_0214@naver.com>
2025-03-20 23:41:57 +0900

    mbtips.kr도메인 허용

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,   // 외부에서 접속 가능하도록 설정
-    strictPort: true
+    strictPort: true,
+    allowedHosts: ['mbtips.kr']
   }
 });
