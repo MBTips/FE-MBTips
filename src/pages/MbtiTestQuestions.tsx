ba6582cd8bc53953b58d697ba15098699c3de6c7
junjeeong <wn8624@naver.com>
2025-03-24 19:34:29 +0900

    feat: Mbti test 과정 페이지 ui 작업 완료

--- src/pages/MbtiTestQuestions.tsx
+++ src/pages/MbtiTestQuestions.tsx
@@ -1,7 +1,35 @@
+import { useParams } from "react-router-dom";
+import { TEST_QNA } from "@/constants/TEST_QNA";
+import MbtiAnswerButtons from "@/components/button/MbtiAnswerButtons";
+
+
+interface info {
+  number: number;
+  question: string;
+  answers: string[];
+}
+
 const MbtiTestQuestions = () => {
-  return (
-    <div>question 페이지</div>
-  )
+
+  const { n } = useParams<{ n: string }>();
+
+  if (n) {
+    const info: info = TEST_QNA[Number(n) - 1];
+
+    return (
+      <main className="flex flex-col justify-center items-center">
+        <span className="font-medium text-gray-500 text-lg">{info.number}/12</span>
+        <h1 className="mt-[20px] font-medium text-3xl text-center whitespace-pre-wrap">{info.question}</h1>
+        {/* 디자인팀에서 이미지 주면 고칠 예정 -> 3.24 정준영 */}
+        <img src="/image/mbti_test.png" alt="mbti 테스트 과정 이미지" width={110} height={129} className="mt-10" />
+        <div className="mt-[93px]">
+          <MbtiAnswerButtons content={info.answers} />
+        </div>
+      </main>
+    )
+  }
+
+  else return <div>404 error</div>
 }
 
 export default MbtiTestQuestions;
