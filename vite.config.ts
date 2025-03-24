c0ab6e36cca65179acc18c8bd79abeabf273c9d5
junjeeong <wn8624@naver.com>
2025-03-24 17:02:49 +0900

    fix: build 에러 고침 -> vite.config + tsconfig.app.json paths 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,31 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// vite에서 @/store/useAuthStore와 같은 절대 경로를 사용하려면, 
+// vite.config.ts 파일에서 alias 설정을 추가해야 합니다.(tsconfig.json을 인식하지 못함)
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: [
       { find: "@/", replacement: path.resolve(__dirname, "src") },
+      { find: "@/api", replacement: path.resolve(__dirname, "src/api") },
       {
         find: "@/components",
         replacement: path.resolve(__dirname, "src/components")
       },
+      { find: "@/hooks", replacement: path.resolve(__dirname, "src/hooks") },
       {
         find: "@/pages",
         replacement: path.resolve(__dirname, "src/pages")
       },
-      { find: "@/hooks", replacement: path.resolve(__dirname, "src/hooks") },
-      { find: "@/utils", replacement: path.resolve(__dirname, "src/utils") },
+      { find: "@/store", replacement: path.resolve(__dirname, "src/store") },
       { find: "@/types", replacement: path.resolve(__dirname, "src/types") },
+      { find: "@/utils", replacement: path.resolve(__dirname, "src/utils") },
       {
         find: "@/constants",
         replacement: path.resolve(__dirname, "src/constants")
       },
-      {
-        find: "@/api",
-        replacement: path.resolve(__dirname, "src/api")
-      }
     ]
   }
 });
