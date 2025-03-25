cba88d5567cb2f48e65b2331e5a108a298b67307
soohyuniii <myungsoocc@gmail.com>
2025-03-25 19:31:49 +0900

    feat: https 띄우기

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import * as path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
-
+import fs from "fs";
 // vite에서 @/store/useAuthStore와 같은 절대 경로를 사용하려면,
 // vite.config.ts 파일에서 alias 설정을 추가해야 합니다.
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mkcert()],
+  server: {
+    https: {
+      key: fs.readFileSync("./certs/key.pem"),
+      cert: fs.readFileSync("./certs/cert.pem")
+    }
+  },
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: [
       { find: "@/", replacement: path.resolve(__dirname, "src") },
