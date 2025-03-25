ce45d0e0449a13c6c4cb4fe1d08fd30e537f1812
junjeeong <wn8624@naver.com>
2025-03-25 13:54:58 +0900

    feat: mbti-test 결과 페이지 ui 작업 완료, 이미지만 추가되면 변경 예정

---
+++ src/components/button/RestartButton.tsx
@@ -0,0 +1,9 @@
+const RestartButton = () => {
+    return (
+        <button className="flex justify-center items-center bg-primary-light rounded-lg w-[72px] h-[60px]">
+            <img src="/icon/restart.svg" alt="다시하기 버튼" width={24} height={24} />
+      </button>
+  )
+}
+
+export default RestartButton;
