7c7a9cb9e9296b8b3bdfc3b4a56dcdf4fe32a4fe
junjeeong <wn8624@naver.com>
2025-03-25 13:54:58 +0900

    feat: mbti-test 결과 페이지 ui 작업 완료, 이미지만 추가되면 변경 예정

--- src/pages/MbtiTestResult.tsx
+++ src/pages/MbtiTestResult.tsx
@@ -2,27 +2,28 @@ import { MouseEvent } from "react";
 import { useParams } from "react-router-dom";
 import { MBTI_RESULT_INFO } from "@/constants/MBTI_RESULT_INFO";
 import ChatStartButton from "@/components/button/ChatStartButton";
+import ShareButton from "@/components/button/ShareButton";
+import RestartButton from "@/components/button/RestartButton";
 
 const MbtiTestResult = () => {
     const { mbti } = useParams<{ mbti: string }>();
     const result = MBTI_RESULT_INFO[mbti?.toUpperCase() as keyof typeof MBTI_RESULT_INFO];
     
-    const handleClick = (e:MouseEvent) => {
+    const handleClick = (e: MouseEvent) => {
         e.preventDefault();
     }
 
-    
     if (!result) return <div>404 error occured</div>;
 
-    else return (
+    return (
         <>
-        <header className="flex justify-center py-[14px] border-gray-100 border-b font-bold text-xl">결과</header>
+            <header className="flex justify-center py-[14px] border-gray-100 border-b font-bold text-xl">결과</header>
             <main className="flex flex-col items-center">
                 <img src="/image/mbti_test_result_image.png" alt="mbti 테스트 결과 이미지" width={335} height={292} className="mt-6"/>
                 <h2 className="mt-6 font-bold text-2xl">{mbti?.toUpperCase()}는 이런 성향이에요!</h2>
                 <ul className="flex gap-3 mt-4">
-                    <li className="rounded-[40px]">{result.tag[0]}</li>
-                    <li className="rounded-[40px]">{result.tag[1]}</li>
+                    <li style={{ backgroundColor: result.bgColor, color: result.textColor }} className="px-4 py-1 rounded-[40px] font-bold">{result.tag[0]}</li>
+                    <li style={{ backgroundColor: result.bgColor, color: result.textColor }} className="px-4 py-1 rounded-[40px] font-bold">{result.tag[1]}</li>
                 </ul>
                 <div className="flex flex-col bg-white mt-4 p-[28px] border-[#EDEDED] border-1 rounded-[20px]">
                     <h3 className="font-bold text-lg">좋아하는 대화 주제</h3>
@@ -38,10 +39,14 @@ const MbtiTestResult = () => {
                 </div>
                 <div className="mt-[22px]">
                     <ChatStartButton onClick={handleClick} />
-                    </div>
+                </div>
+                <div className="flex gap-4 mt-5 w-full">
+                    <ShareButton />
+                    <RestartButton/>
+                </div>
             </main>
         </>
-  )
+    );
 }
 
 export default MbtiTestResult;
