1caa930359aa1fd53a2818e3f79f50b9263538a0
junjeeong <wn8624@naver.com>
2025-03-25 13:26:29 +0900

    feat: mbti-test 결과 페이지 ui 작업 완료, 이미지만 추가되면 변경 예정

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Login from "@/pages/Login";
 import KaKaoLogin from "@/pages/KaKaoLogin";
 import MbtiTestIntro from "@/pages/MbtiTestIntro";
 import MbtiTestQuestions from "@/pages/MbtiTestQuestions";
+import MbtiTestResult from "@/pages/MbtiTestResult";
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
         <Route path="/kakao-login" element={<KaKaoLogin />} />
         <Route path="/mbti-test" element={<MbtiTestIntro />} />
         <Route path="/mbti-test/:n" element={<MbtiTestQuestions />} />
+        <Route path="/mbti-test-result/:mbti" element={<MbtiTestResult/>}/>
       </Routes>
     </Router>
   );
--- src/pages/MbtiTestQuestions.tsx
+++ src/pages/MbtiTestQuestions.tsx
@@ -17,15 +17,15 @@ const MbtiTestQuestions = () => {
     const info: info = TEST_QNA[Number(n) - 1];
 
     return (
-      <main className="flex flex-col justify-center items-center">
-        <span className="font-medium text-gray-500 text-lg">{info.number}/12</span>
-        <h1 className="mt-[20px] font-medium text-3xl text-center whitespace-pre-wrap">{info.question}</h1>
-        {/* 디자인팀에서 이미지 주면 고칠 예정 -> 3.24 정준영 */}
-        <img src="/image/mbti_test.png" alt="mbti 테스트 과정 이미지" width={110} height={129} className="mt-10" />
-        <div className="mt-[93px]">
-          <MbtiAnswerButtons content={info.answers} />
-        </div>
-      </main>
+        <main className="flex flex-col justify-center items-center">
+          <span className="font-medium text-gray-500 text-lg">{info.number}/12</span>
+          <h1 className="mt-[20px] font-medium text-3xl text-center whitespace-pre-wrap">{info.question}</h1>
+          {/* 디자인팀에서 이미지 주면 고칠 예정 -> 3.24 정준영 */}
+          <img src="/image/mbti_test.png" alt="mbti 테스트 과정 이미지" width={110} height={129} className="mt-10" />
+          <div className="mt-[93px]">
+            <MbtiAnswerButtons content={info.answers} />
+          </div>
+        </main>
     )
   }
 
