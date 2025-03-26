4c0c987cccd106d1cd749d9cfc93022afb56541e
yuhojae <ho_0214@naver.com>
2025-03-26 21:51:41 +0900

    테스트

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,10 @@ export default defineConfig({
     port: 3000,
     host: true,   // 외부에서 접속 가능하도록 설정
     strictPort: true,
-    allowedHosts: ['mbtips.kr']
+    allowedHosts: ['mbtips.kr'],
+    hmr: {
+      host: 'mbtips.kr',
+      protocol: 'wss'
+    }
   }
 });
