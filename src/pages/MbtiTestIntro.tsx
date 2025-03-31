a391c8fe666e1e6540683d5bb9e6c1d10e0eeaae
junjeong <wn8624@naver.com>
2025-03-31 20:52:33 +0900

    fix: mbti test intro 페이지 이미지 변경

--- src/pages/MbtiTestIntro.tsx
+++ src/pages/MbtiTestIntro.tsx
@@ -26,29 +26,28 @@ const MbtiTestIntro = () => {
   return (
     <main className="md:w-[360px] md:h-[812px]">
       <NavBar />
-      <div className="flex flex-col items-center bg-primary-light w-full h-full">
-        <span className="mt-[38px] font-medium text-lg">그 사람의 mbti는 뭘까?</span>
-        <h2 className="mt-[12px] font-extrabold text-[32px] text-center">
+      <div className="relative flex flex-col items-center w-full h-[756px] bg-[url('/image/mbti_test_intro.png')] bg-cover bg-no-repeat">
+        <span className="absolute top-[38px] font-medium text-lg">그 사람의 mbti는 뭘까?</span>
+        <h2 className="absolute top-[74px] font-extrabold text-[32px] text-center">
           <span className="text-[#2714FF]">상대방</span> MBTI
           <br />
           유추 테스트
         </h2>
-        <img src="/image/Rectangle.png" alt=" mbti 유추 이미지" className="mt-[28px]" width={160} height={225}/>
-        <form className="flex flex-col items-center mt-[120px]" onSubmit={handleSubmit}>
-          <label htmlFor="name" className="font-medium text-lg">MBTI를 알고 싶은 상대의 이름</label>
+        <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+          <label htmlFor="name" className="font-medium text-lg absolute bottom-[257px]">MBTI를 알고 싶은 상대의 이름</label>
           <input
             type="text"
             id="name"
             onChange={handleChange}
-            className="bg-white mt-[20px] rounded-lg w-[320px] h-[68px] text-center"
+            className="bg-white rounded-lg w-[320px] h-[68px] text-center absolute bottom-[172px]"
           />
           <button
             type="submit"
-            className="bg-primary-normal hover:opacity-80 mt-10 rounded-lg w-[320px] h-[60px] font-bold text-white"
+            className="bg-primary-normal hover:opacity-80 absolute bottom-[49px] rounded-lg w-[320px] h-[60px] font-bold text-white"
           >
             시작하기
           </button>
-        </form>
+        </form> 
       </div>
     </main>
   )
