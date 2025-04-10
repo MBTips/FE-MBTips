b19b76f85300629ca310bf89a15f8393d2ba3d90
junjeong <wn8624@naver.com>
2025-04-10 13:56:22 +0900

    working

--- src/components/modal/TermsAndPrivacyModal.tsx
+++ src/components/modal/TermsAndPrivacyModal.tsx
@@ -14,17 +14,19 @@ const TermsAndPrivacyModal = ({
       : " 나 개인정보처리방침 블라블라블라";
 
   return (
-    <div className="z-1 fixed inset-0 flex flex-col items-center bg-white px-[45px] py-[25px] w-screen h-screen">
+    <div className="inset-0 fixed flex items-center justify-center  z-50 bg-black/50">
+    <div className="relative rounded-lg flex flex-col items-center w-[331px] h-[476px] bg-white px-[45px] py-[25px] overflow-scroll">
       <h1 className="font-bold text-2xl">{title}</h1>
       <p className="mt-[30px]">{description}</p>
       <button
         onClick={() =>
           closeModal({ isOpen: false, terms: false, privacy: false })
         }
-        className="top-4 right-4 fixed"
+        className="absolute top-4 right-4"
       >
-        <img src="/icon/close.svg" alt="닫기 버튼" width={28} height={28} />
+        <img src="/icon/close.svg" alt="닫기 버튼" width={18} height={18} />
       </button>
+    </div>
     </div>
   );
 };
