d381e6761e0d0f90083c1e8ff889f88b6c879fd4
soohyuniii <myungsoocc@gmail.com>
2025-04-10 16:05:20 +0900

    feat: myinfo 화면 내 terms, privacy modal 연동

--- src/components/modal/ActionConfirmModal.tsx
+++ src/components/modal/ActionConfirmModal.tsx
@@ -18,7 +18,10 @@ const ActionConfirmModal = ({
   onConfirm
 }: AlertProps) => {
   return (
-    <div className="absolute inset-0 z-50 flex items-center justify-center bg-[#00000080]">
+    <div
+      className="absolute inset-0 z-50 flex items-center justify-center bg-black/50"
+      onClick={onCancel}
+    >
       <div className="flex h-[200px] w-[294px] flex-col items-center justify-center rounded-2xl bg-white">
         <h3 className="text-2xl font-bold">{title}</h3>
         <p className="mt-[9px] text-center text-xl font-medium">
