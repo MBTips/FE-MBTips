6e610541f9c828767fa1498458914b1fd0f2d923
junjeong <wn8624@naver.com>
2025-04-10 13:35:41 +0900

    style: MainHeader items-center 적용

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -3,7 +3,7 @@ import LoginButton from "../button/LoginButton";
 
 const MainHeader = ({isLoggedIn}: {isLoggedIn : boolean})=> {
   return (
-    <header className="bg-white flex h-[56px] w-full justify-between px-5">
+    <header className="bg-white flex h-[56px] w-full justify-between items-center px-5">
       <Link to="/" className="flex items-center">
       <img
         src="/public/icon/mbtipslogo.svg"
