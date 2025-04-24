fa833300fe390d61ce378c22fd77d6fb6395e01c
junjeong <wn8624@naver.com>
2025-04-24 17:01:08 +0900

    Mbti intro 페이지 시작하기 버튼, Mbti 결과 페이지 돌아가기 버튼+공유하기 버튼 Click 이벤트 추가

--- src/components/button/ShareButton.tsx
+++ src/components/button/ShareButton.tsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import ShareModal from "@/components/modal/ShareModal";
+import trackClickEvent from "@/utils/trackClickEvent";
 
 const ShareButton = () => {
   const [shareModalIsOpen, setShareModalIsOpen] = useState(false);
 
+  const handleClick = () => {
+    setShareModalIsOpen(true);
+    trackClickEvent("MBTI 테스트 결과", "공유하기 버튼");
+  };
   return (
     <>
-      <button onClick={()=>setShareModalIsOpen(true)} className="flex h-[60px] w-full items-center justify-center rounded-lg border border-primary-light bg-primary-pale font-bold text-primary-normal">
+      <button
+        onClick={handleClick}
+        className="flex h-[60px] w-full items-center justify-center rounded-lg border border-primary-light bg-primary-pale font-bold text-primary-normal"
+      >
         공유하기
       </button>
       {shareModalIsOpen && (
