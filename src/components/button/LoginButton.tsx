9bb58bf21f522ecabead1bfd624f2ddd6d562c6b
junjeong <wn8624@naver.com>
2025-04-24 17:08:36 +0900

    홈 화면 로그인 버튼 click 이벤트 할당

--- src/components/button/LoginButton.tsx
+++ src/components/button/LoginButton.tsx
@@ -1,8 +1,14 @@
+import trackClickEvent from "@/utils/trackClickEvent";
+
 const LoginButton = () => {
+  const handleClick = () => {
+    trackClickEvent("홈", "로그인 버튼");
+  };
+
   return (
-    <a href="/login">
-      <button className="w-[69px] h-[36px] border-[0.8px] border-gray-300 rounded-lg font-bold text-gray-600 text-md">
-      로그인
+    <a href="/login" onClick={handleClick}>
+      <button className="h-[36px] w-[69px] rounded-lg border-[0.8px] border-gray-300 text-md font-bold text-gray-600">
+        로그인
       </button>
     </a>
   );
