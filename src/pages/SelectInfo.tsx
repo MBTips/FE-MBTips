da7ab0040f38479f00438096f086a331d172198d
권한있는-유저네임 <myungsoocc@gmail.com>
2025-07-22 10:26:54 +0900

    fix: conflict 해결

--- src/pages/SelectInfo.tsx
+++ src/pages/SelectInfo.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import FormButton from "@/components/button/FormButton";
 import Header from "@/components/header/Header";
 import { getMBTIgroup, mapAgeToNumber } from "@/utils/helpers";
@@ -257,185 +258,222 @@ const SelectInfo = () => {
   };
 
   return (
-    <div className="flex min-h-screen w-[360px] flex-col bg-white md:w-[375px] lg:w-[500px]">
-      <Header title={headerTitle} showShareIcon={false} />
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content={
+            type === "fastFriend"
+              ? "상대방 정보 설정"
+              : type === "virtualFriend"
+                ? "친구 정보 저장"
+                : "오픈채팅 정보 설정"
+          }
+        />
+        <meta
+          property="og:description"
+          content={
+            type === "fastFriend"
+              ? "상대방 정보 설정"
+              : type === "virtualFriend"
+                ? "친구 정보 저장"
+                : "오픈채팅 정보 설정"
+          }
+        />
+        <meta
+          property="twitter:description"
+          content={
+            type === "fastFriend"
+              ? "상대방 정보 설정"
+              : type === "virtualFriend"
+                ? "친구 정보 저장"
+                : "오픈채팅 정보 설정"
+          }
+        />
+      </Helmet>
 
-      <div className="mx-auto w-[320px]">
-        {/* MBTI 선택 */}
-        <div className="mb-[40px] pt-[48px]">
-          <p className="text-[20px] leading-[30px] font-bold tracking-[-0.01em] whitespace-pre-line">
-            {selectInfoTitle}
-          </p>
+      <div className="flex min-h-screen w-[360px] flex-col bg-white md:w-[375px] lg:w-[500px]">
+        <Header title={headerTitle} showShareIcon={false} />
 
-          <div className="grid grid-cols-4 gap-[24px_13px] pt-[24px]">
-            {mbtiOptions.map((option) => (
-              <FormButton
-                key={option}
-                size="md"
-                selected={isMBTISelected(option)}
-                onClick={() => handleMBTISelect(option)}
-              >
-                {option}
-              </FormButton>
-            ))}
-          </div>
-        </div>
-      </div>
-
-      {/* 구분선 */}
-      <div className="h-[8px] w-full bg-[#EEF0F3]" />
-
-      {!isTopicChat && (
         <div className="mx-auto w-[320px]">
-          <div className="pt-[40px]">
-            <p className="text-[20px] leading-[30px] font-bold tracking-[-0.01em]">
-              정보 추가 입력
+          {/* MBTI 선택 */}
+          <div className="mb-[40px] pt-[100px]">
+            <p className="text-[20px] leading-[30px] font-bold tracking-[-0.01em] whitespace-pre-line">
+              {selectInfoTitle}
             </p>
 
-            {/* 이름 입력 */}
-            <div className="flex flex-col gap-2 pt-[32px]">
-              <label
-                htmlFor="name"
-                className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600"
-              >
-                이름
-                {isNameRequired && <span className="text-red-500">*</span>}
-              </label>
-              <input
-                id="name"
-                type="text"
-                value={name}
-                onChange={handleNameChange}
-                className="h-[56px] w-full rounded-lg border border-gray-200 px-4 focus:border-primary-light focus:ring-primary-light focus:outline-none"
-                placeholder="이름"
-                maxLength={6}
-              />
-            </div>
-
-            {/* 나이 선택 */}
-            <div className="pt-[20px] pb-[12px]">
-              <p className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600">
-                나이
-              </p>
-              <div className="flex gap-[16px] pt-[16px]">
-                {ageOptions.map((option) => (
-                  <FormButton
-                    key={option}
-                    size="sm"
-                    selected={age === option}
-                    onClick={() => handleButtonClick(option, setAge, age)}
-                  >
-                    {option}
-                  </FormButton>
-                ))}
-              </div>
-            </div>
-
-            {/* 성별 선택 */}
-            <div className="pt-[20px] pb-[12px]">
-              <p className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600">
-                성별
-              </p>
-              <div className="flex gap-[16px] pt-[16px]">
-                {genderOptions.map((option) => (
-                  <FormButton
-                    key={option}
-                    size="sm"
-                    selected={gender === option}
-                    onClick={() => handleButtonClick(option, setGender, gender)}
-                  >
-                    {option}
-                  </FormButton>
-                ))}
-              </div>
-            </div>
-
-            {/* 관계 선택 */}
-            <div className="pt-[20px] pb-[20px]">
-              <p className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600">
-                상대방과 나의 관계
-              </p>
-              <div className="grid grid-cols-4 gap-[16px] pt-[16px]">
-                {relationshipOptions.map((option) => (
-                  <FormButton
-                    key={option}
-                    size="sm"
-                    selected={relationship === option}
-                    onClick={() =>
-                      handleButtonClick(option, setRelationship, relationship)
-                    }
-                  >
-                    {option}
-                  </FormButton>
-                ))}
-              </div>
-            </div>
-
-            {/* 관심사 선택 */}
-            <div className="pt-[20px] pb-[26px]">
-              <p className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600">
-                관심사
-              </p>
-              <div className="grid grid-cols-4 gap-[16px] pt-[16px]">
-                {interestOptions.map((option) => (
-                  <FormButton
-                    key={option}
-                    size="sm"
-                    selected={isInterestSelected(option)}
-                    onClick={() => handleInterestSelect(option)}
-                  >
-                    {option}
-                  </FormButton>
-                ))}
-              </div>
+            <div className="grid grid-cols-4 gap-[24px_13px] pt-[24px]">
+              {mbtiOptions.map((option) => (
+                <FormButton
+                  key={option}
+                  size="md"
+                  selected={isMBTISelected(option)}
+                  onClick={() => handleMBTISelect(option)}
+                >
+                  {option}
+                </FormButton>
+              ))}
             </div>
           </div>
         </div>
-      )}
 
-      {/* topicChat일 때만 이름 입력 필드 표시 */}
-      {isTopicChat && (
-        <div className="mx-auto w-[320px]">
-          <div className="pt-[40px]">
-            {/* 이름 입력 */}
-            <div className="flex flex-col gap-2">
-              <label
-                htmlFor="name"
-                className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600"
-              >
-                이름
-              </label>
-              <input
-                id="name"
-                type="text"
-                value={name}
-                onChange={handleNameChange}
-                className="h-[56px] w-full rounded-lg border border-gray-200 px-4 focus:border-primary-light focus:ring-primary-light focus:outline-none"
-                placeholder="이름"
-                maxLength={6}
-              />
+        {/* 구분선 */}
+        <div className="h-[8px] w-full bg-[#EEF0F3]" />
+
+        {!isTopicChat && (
+          <div className="mx-auto w-[320px]">
+            <div className="pt-[40px]">
+              <p className="text-[20px] leading-[30px] font-bold tracking-[-0.01em]">
+                정보 추가 입력
+              </p>
+
+              {/* 이름 입력 */}
+              <div className="flex flex-col gap-2 pt-[32px]">
+                <label
+                  htmlFor="name"
+                  className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600"
+                >
+                  이름
+                  {isNameRequired && <span className="text-red-500">*</span>}
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  value={name}
+                  onChange={handleNameChange}
+                  className="h-[56px] w-full rounded-lg border border-gray-200 px-4 focus:border-primary-light focus:ring-primary-light focus:outline-none"
+                  placeholder="이름"
+                  maxLength={6}
+                />
+              </div>
+
+              {/* 나이 선택 */}
+              <div className="pt-[20px] pb-[12px]">
+                <p className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600">
+                  나이
+                </p>
+                <div className="flex gap-[16px] pt-[16px]">
+                  {ageOptions.map((option) => (
+                    <FormButton
+                      key={option}
+                      size="sm"
+                      selected={age === option}
+                      onClick={() => handleButtonClick(option, setAge, age)}
+                    >
+                      {option}
+                    </FormButton>
+                  ))}
+                </div>
+              </div>
+
+              {/* 성별 선택 */}
+              <div className="pt-[20px] pb-[12px]">
+                <p className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600">
+                  성별
+                </p>
+                <div className="flex gap-[16px] pt-[16px]">
+                  {genderOptions.map((option) => (
+                    <FormButton
+                      key={option}
+                      size="sm"
+                      selected={gender === option}
+                      onClick={() =>
+                        handleButtonClick(option, setGender, gender)
+                      }
+                    >
+                      {option}
+                    </FormButton>
+                  ))}
+                </div>
+              </div>
+
+              {/* 관계 선택 */}
+              <div className="pt-[20px] pb-[20px]">
+                <p className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600">
+                  상대방과 나의 관계
+                </p>
+                <div className="grid grid-cols-4 gap-[16px] pt-[16px]">
+                  {relationshipOptions.map((option) => (
+                    <FormButton
+                      key={option}
+                      size="sm"
+                      selected={relationship === option}
+                      onClick={() =>
+                        handleButtonClick(option, setRelationship, relationship)
+                      }
+                    >
+                      {option}
+                    </FormButton>
+                  ))}
+                </div>
+              </div>
+
+              {/* 관심사 선택 */}
+              <div className="pt-[20px] pb-[26px]">
+                <p className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600">
+                  관심사
+                </p>
+                <div className="grid grid-cols-4 gap-[16px] pt-[16px]">
+                  {interestOptions.map((option) => (
+                    <FormButton
+                      key={option}
+                      size="sm"
+                      selected={isInterestSelected(option)}
+                      onClick={() => handleInterestSelect(option)}
+                    >
+                      {option}
+                    </FormButton>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
+        )}
+
+        {/* topicChat일 때만 이름 입력 필드 표시 */}
+        {isTopicChat && (
+          <div className="mx-auto w-[320px]">
+            <div className="pt-[40px]">
+              {/* 이름 입력 */}
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="name"
+                  className="text-2lg leading-[24px] font-bold tracking-[0em] text-gray-600"
+                >
+                  이름
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  value={name}
+                  onChange={handleNameChange}
+                  className="h-[56px] w-full rounded-lg border border-gray-200 px-4 focus:border-primary-light focus:ring-primary-light focus:outline-none"
+                  placeholder="이름"
+                  maxLength={6}
+                />
+              </div>
+            </div>
+          </div>
+        )}
+
+        {toastMessage && (
+          <ToastMessage
+            message={toastMessage}
+            onClose={() => setToastMessage(null)}
+          />
+        )}
+
+        {/* 대화 시작 버튼 */}
+        <div className="mx-auto mt-auto mb-[17px] w-[320px]">
+          <button
+            className="h-[60px] w-full rounded-[8px] bg-primary-normal font-bold text-white"
+            onClick={handleConfirmButton}
+          >
+            {confirmButtonText}
+          </button>
         </div>
-      )}
-
-      {toastMessage && (
-        <ToastMessage
-          message={toastMessage}
-          onClose={() => setToastMessage(null)}
-        />
-      )}
-
-      {/* 대화 시작 버튼 */}
-      <div className="mx-auto mt-auto mb-[17px] w-[320px]">
-        <button
-          className="h-[60px] w-full rounded-[8px] bg-primary-normal font-bold text-white"
-          onClick={handleConfirmButton}
-        >
-          {confirmButtonText}
-        </button>
       </div>
-    </div>
+    </>
   );
 };
 
